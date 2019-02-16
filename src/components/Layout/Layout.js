f61e48b8d93a034f3b4b45a908490b03fa1a0cc7
rafael cervantes <rafael@vipmarketingla.com>
2019-02-16 08:04:27 -0800

    commit 02-19

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import Aux from '../../hoc/_Aux';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
 );
-
 export default layout;
